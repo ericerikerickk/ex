ad84b628e5de106795092810dbd0343f27447383
Eric Poblete <ericpoblete123@gmail.com>
2024-03-07 10:16:56 +0800

    userTable Script

---
+++ ex/Create dbo.userTable.sql
@@ -0,0 +1,17 @@
+﻿USE [master]
+GO
+
+/****** Object: Table [dbo].[userTable] Script Date: 3/7/2024 10:16:06 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[userTable] (
+    [userID]   INT            IDENTITY (1, 1) NOT NULL,
+    [userName] NVARCHAR (50)  NULL,
+    [password] NVARCHAR (100) NULL
+);
+
+
