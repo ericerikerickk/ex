38769d03a9633c0b87848ad54654bae892a52aac
Kervie19 <kerviemille@gmail.com>
2024-03-08 11:46:59 +0800

    doc

--- ex/Documents.Designer.cs
+++ ex/Documents.Designer.cs
@@ -30,27 +30,39 @@
         {
             this.panelDocuments = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panelDocuments.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelDocuments
             // 
             this.panelDocuments.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.panelDocuments.Controls.Add(this.label1);
-            this.panelDocuments.Location = new System.Drawing.Point(43, 177);
+            this.panelDocuments.Controls.Add(this.dataGridView1);
+            this.panelDocuments.Location = new System.Drawing.Point(30, 177);
             this.panelDocuments.Name = "panelDocuments";
-            this.panelDocuments.Size = new System.Drawing.Size(889, 154);
+            this.panelDocuments.Size = new System.Drawing.Size(902, 202);
             this.panelDocuments.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(347, 56);
+            this.label1.Location = new System.Drawing.Point(48, 109);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(238, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "WAY SHAT DIHA?";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 20);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(862, 159);
+            this.dataGridView1.TabIndex = 1;
             // 
             // Documents
             // 
@@ -58,14 +70,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(974, 509);
             this.Controls.Add(this.panelDocuments);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Documents";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Documents";
             this.panelDocuments.ResumeLayout(false);
-            this.panelDocuments.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,5 +87,6 @@
 
         private System.Windows.Forms.Panel panelDocuments;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
