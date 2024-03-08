3cd19da0a9e5f20abac752d654d9b9f222b4ff7f
Kervie19 <kerviemille@gmail.com>
2024-03-08 15:36:52 +0800

    steps234

---
+++ ex/STEP4.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ex
+{
+    partial class STEP4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSTEP4 = new System.Windows.Forms.Panel();
+            this.dataGridSTEP4 = new System.Windows.Forms.DataGridView();
+            this.panelSTEP4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSTEP4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelSTEP4
+            // 
+            this.panelSTEP4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panelSTEP4.Controls.Add(this.dataGridSTEP4);
+            this.panelSTEP4.Location = new System.Drawing.Point(36, 153);
+            this.panelSTEP4.Name = "panelSTEP4";
+            this.panelSTEP4.Size = new System.Drawing.Size(902, 202);
+            this.panelSTEP4.TabIndex = 4;
+            // 
+            // dataGridSTEP4
+            // 
+            this.dataGridSTEP4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridSTEP4.Location = new System.Drawing.Point(23, 20);
+            this.dataGridSTEP4.Name = "dataGridSTEP4";
+            this.dataGridSTEP4.RowHeadersWidth = 51;
+            this.dataGridSTEP4.RowTemplate.Height = 24;
+            this.dataGridSTEP4.Size = new System.Drawing.Size(862, 159);
+            this.dataGridSTEP4.TabIndex = 1;
+            // 
+            // STEP4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 509);
+            this.Controls.Add(this.panelSTEP4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "STEP4";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "STEP4";
+            this.panelSTEP4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridSTEP4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSTEP4;
+        private System.Windows.Forms.DataGridView dataGridSTEP4;
+    }
+}
