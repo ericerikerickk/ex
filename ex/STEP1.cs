a5258315303c815eba0a9b1470113e45e144abe6
Kervie19 <kerviemille@gmail.com>
2024-03-08 14:55:15 +0800

    added forms for every steps

---
+++ ex/STEP1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex
+{
+    public partial class STEP1 : Form
+    {
+        public STEP1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ex/STEP4.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ex
+{
+    partial class STEP4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewSTEP4 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSTEP4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewSTEP4
+            // 
+            this.dataGridViewSTEP4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridViewSTEP4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSTEP4.Location = new System.Drawing.Point(56, 202);
+            this.dataGridViewSTEP4.Name = "dataGridViewSTEP4";
+            this.dataGridViewSTEP4.RowHeadersWidth = 51;
+            this.dataGridViewSTEP4.RowTemplate.Height = 24;
+            this.dataGridViewSTEP4.Size = new System.Drawing.Size(955, 150);
+            this.dataGridViewSTEP4.TabIndex = 3;
+            // 
+            // STEP4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.dataGridViewSTEP4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "STEP4";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "STEP4";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSTEP4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewSTEP4;
+    }
+}
