1d2a9befe1788d631a385109c5c0339ed2c263c5
Kervie19 <kerviemille@gmail.com>
2024-03-14 13:20:42 +0800

    STEP1 Dtagrid

--- ex/Documents.Designer.cs
+++ ex/Documents.Designer.cs
@@ -55,37 +55,37 @@
             this.panelDocuments.Controls.Add(this.dataGridViewDocs);
             this.panelDocuments.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelDocuments.Location = new System.Drawing.Point(0, 0);
-            this.panelDocuments.Margin = new System.Windows.Forms.Padding(2);
+            this.panelDocuments.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelDocuments.Name = "panelDocuments";
-            this.panelDocuments.Size = new System.Drawing.Size(730, 414);
+            this.panelDocuments.Size = new System.Drawing.Size(973, 510);
             this.panelDocuments.TabIndex = 0;
             // 
             // lblHello
             // 
             this.lblHello.AutoSize = true;
-            this.lblHello.Location = new System.Drawing.Point(79, 133);
+            this.lblHello.Location = new System.Drawing.Point(105, 164);
+            this.lblHello.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblHello.Name = "lblHello";
-            this.lblHello.Size = new System.Drawing.Size(35, 13);
+            this.lblHello.Size = new System.Drawing.Size(44, 16);
             this.lblHello.TabIndex = 13;
             this.lblHello.Text = "label3";
             // 
             // txtProjectNo
             // 
-            this.txtProjectNo.Location = new System.Drawing.Point(174, 18);
-            this.txtProjectNo.Margin = new System.Windows.Forms.Padding(2);
+            this.txtProjectNo.Location = new System.Drawing.Point(232, 22);
+            this.txtProjectNo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtProjectNo.Multiline = true;
             this.txtProjectNo.Name = "txtProjectNo";
-            this.txtProjectNo.Size = new System.Drawing.Size(145, 28);
+            this.txtProjectNo.Size = new System.Drawing.Size(192, 34);
             this.txtProjectNo.TabIndex = 12;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(353, 23);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(471, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.Size = new System.Drawing.Size(140, 20);
             this.label1.TabIndex = 11;
             this.label1.Text = "PROJECT NAME";
             // 
@@ -95,10 +95,10 @@
             this.btnSubmitdocs.FlatAppearance.BorderSize = 0;
             this.btnSubmitdocs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSubmitdocs.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.btnSubmitdocs.Location = new System.Drawing.Point(283, 156);
-            this.btnSubmitdocs.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSubmitdocs.Location = new System.Drawing.Point(377, 192);
+            this.btnSubmitdocs.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSubmitdocs.Name = "btnSubmitdocs";
-            this.btnSubmitdocs.Size = new System.Drawing.Size(148, 35);
+            this.btnSubmitdocs.Size = new System.Drawing.Size(197, 43);
             this.btnSubmitdocs.TabIndex = 10;
             this.btnSubmitdocs.Text = "SUBMIT";
             this.btnSubmitdocs.UseVisualStyleBackColor = false;
@@ -106,30 +106,29 @@
             // 
             // txtProjectDescription
             // 
-            this.txtProjectDescription.Location = new System.Drawing.Point(176, 67);
-            this.txtProjectDescription.Margin = new System.Windows.Forms.Padding(2);
+            this.txtProjectDescription.Location = new System.Drawing.Point(235, 82);
+            this.txtProjectDescription.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtProjectDescription.Multiline = true;
             this.txtProjectDescription.Name = "txtProjectDescription";
-            this.txtProjectDescription.Size = new System.Drawing.Size(330, 79);
+            this.txtProjectDescription.Size = new System.Drawing.Size(439, 96);
             this.txtProjectDescription.TabIndex = 5;
             // 
             // txtProjectName
             // 
-            this.txtProjectName.Location = new System.Drawing.Point(469, 18);
-            this.txtProjectName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtProjectName.Location = new System.Drawing.Point(625, 22);
+            this.txtProjectName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtProjectName.Multiline = true;
             this.txtProjectName.Name = "txtProjectName";
-            this.txtProjectName.Size = new System.Drawing.Size(145, 28);
+            this.txtProjectName.Size = new System.Drawing.Size(192, 34);
             this.txtProjectName.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(8, 68);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(11, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(164, 16);
+            this.label2.Size = new System.Drawing.Size(205, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "PROJECT DESCRIPTION";
             // 
@@ -137,10 +136,9 @@
             // 
             this.lblProject.AutoSize = true;
             this.lblProject.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProject.Location = new System.Drawing.Point(38, 23);
-            this.lblProject.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblProject.Location = new System.Drawing.Point(51, 28);
             this.lblProject.Name = "lblProject";
-            this.lblProject.Size = new System.Drawing.Size(132, 16);
+            this.lblProject.Size = new System.Drawing.Size(165, 20);
             this.lblProject.TabIndex = 2;
             this.lblProject.Text = "PROJECT NUMBER";
             // 
@@ -148,23 +146,23 @@
             // 
             this.dataGridViewDocs.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.dataGridViewDocs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewDocs.Location = new System.Drawing.Point(12, 201);
-            this.dataGridViewDocs.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewDocs.Location = new System.Drawing.Point(87, 247);
+            this.dataGridViewDocs.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridViewDocs.Name = "dataGridViewDocs";
             this.dataGridViewDocs.ReadOnly = true;
             this.dataGridViewDocs.RowHeadersWidth = 51;
             this.dataGridViewDocs.RowTemplate.Height = 24;
-            this.dataGridViewDocs.Size = new System.Drawing.Size(689, 180);
+            this.dataGridViewDocs.Size = new System.Drawing.Size(799, 222);
             this.dataGridViewDocs.TabIndex = 1;
             // 
             // Documents
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(730, 414);
+            this.ClientSize = new System.Drawing.Size(973, 510);
             this.Controls.Add(this.panelDocuments);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Documents";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
